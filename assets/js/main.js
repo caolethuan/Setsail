9ff8cb68d54967975ca314724f9feb011104bb12
Thuần Cao <1951120153@sv.ut.edu.vn>
2021-12-19 10:09:30 +0700

    Header, modal JS

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+var avt_btn = $('.header__user-avt')
+var modal = $('.modal')
+var modal_overlay = $('.modal__overlay')
+var modal_selected = $$('.auth-form__switch-modal')
+var modal_list = $$('.auth-form')
+
+showModal = () => {
+    modal.classList.add('open')
+}
+
+hideModal = (e) => {
+    // e.stopPropagation()
+    modal.classList.remove('open')
+}
+
+switchModal = () => {
+    Array.from(modal_list).forEach(item => {
+        item.classList.toggle('auth-form-open')
+    })
+}
+
+avt_btn.addEventListener('click', showModal)
+modal_overlay.addEventListener('click', hideModal)
+
+Array.from(modal_selected).forEach(modal => modal.addEventListener('click', switchModal))
+
+
