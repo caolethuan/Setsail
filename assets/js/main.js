0acfd6631cbaa36e3fed69d5b1772b9c8c05f0a8
Thuần Cao <1951120153@sv.ut.edu.vn>
2021-12-23 11:51:04 +0700

    Responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
 
-var avt_btn = $('.header__user-avt')
+var avt_btn = $$('.header__user-avt')
 var modal = $('.modal')
 var modal_overlay = $('.modal__overlay')
 var modal_selected = $$('.auth-form__switch-modal')
@@ -22,7 +22,9 @@ switchModal = () => {
     })
 }
 
-avt_btn.addEventListener('click', showModal)
+Array.from(avt_btn).forEach(btn => {
+    btn.addEventListener('click', showModal)  
+})
 modal_overlay.addEventListener('click', hideModal)
 
 Array.from(modal_selected).forEach(modal => modal.addEventListener('click', switchModal))
